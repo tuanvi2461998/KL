a2da7730ae1b24fb0978db0200079e6009ddd6ec
Nguyễn Tuấn Vĩ <58478736+tuanvi2461998@users.noreply.github.com>
2020-06-01 10:22:33 +0700

    viewcomponent

---
+++ KhoaLuanCoreApp/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KhoaLuanCoreApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        [Route("san-pham.html")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Catalog(int id, string keyword, int? pageSize, string sortBy, int page=1)
+        {
+            return View();
+        }
+    }
+}
